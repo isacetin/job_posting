7a3e30a55c2978d1be4986d8c537212bde25070e
isa Çetin <isa.cetin@gelirler.gov.tr>
2024-11-18 16:19:11 +0300

    progress indicator dioalog haline getirildi

--- app/src/main/java/com/isacetin/jopposting/components/progressIndicator/JobProgressIndicator.kt
+++ app/src/main/java/com/isacetin/jopposting/components/progressIndicator/JobProgressIndicator.kt
@@ -1,30 +1,46 @@
 package com.isacetin.jopposting.components.progressIndicator
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import com.isacetin.jopposting.ui.theme.Primary
-import com.isacetin.jopposting.ui.theme.Secondary
 
 @Composable
 fun JobProgressIndicator() {
-    Box(
-        modifier =
-            Modifier
-                .fillMaxSize()
-                .background(color = Secondary.copy(alpha = 0.1f))
-                .clickable(enabled = false) {},
-        contentAlignment = Alignment.Center
+    Dialog(
+        onDismissRequest = {}
     ) {
-        CircularProgressIndicator(
-            color = Primary
-        )
+        Card(
+            modifier =
+                Modifier
+                    .background(color = Color.Transparent)
+                    .size(75.dp)
+                    .clip(RoundedCornerShape(12.dp))
+        ) {
+            Box(
+                modifier =
+                    Modifier
+                        .fillMaxSize()
+                        .background(Color.White, shape = RoundedCornerShape(12.dp)),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator(
+                    color = Primary
+                )
+            }
+        }
     }
 }
 
